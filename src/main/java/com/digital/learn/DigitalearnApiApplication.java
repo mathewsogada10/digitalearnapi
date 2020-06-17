d59740fdc681a2edf63f60cb04188dc8a72a48b2
mathewsogada10 <40469270+mathewsogada10@users.noreply.github.com>
2020-06-17 19:55:38 +0300

    initial commit

---
+++ src/main/java/com/digital/learn/DigitalearnApiApplication.java
@@ -0,0 +1,13 @@
+package com.digital.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalearnApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalearnApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digital/learn/DigitalearnApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digital.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalearnApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
